13107e2ae5b7504a0e6debd91bb85f716c87e399
Pratham Jain <prathamjainn@gmail.com>
2023-08-29 15:37:55 +0530

    sdfsd

---
+++ mainScript.js
@@ -0,0 +1,18 @@
+
+console.log('mainscript is working');
+
+
+function typedjs()
+{
+    var typed = new Typed('#designations', {
+        strings: ['Python Developer','Web  Developer ','Blogger ','Youtuber', ' Learner' , " Machine Learning Engineer"],
+        typeSpeed: 40,
+        backSpeed: 20,
+        loop: true,
+        // shuffle: true,
+        cursorChar: '✒️',
+        bindInputFocusEvents: true, 
+        contentType: 'null',
+      });
+}
+
