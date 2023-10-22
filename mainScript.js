066d3f4d31e22f1671a83e5034306225266794f7
Pratham Jain <prathamjainn@gmail.com>
2023-10-22 19:36:51 +0530

    no

--- mainScript.js
+++ mainScript.js
@@ -44,11 +44,65 @@ function crousals()
   });
 }
 
+function crousals2()
+{
+  var swiper = new Swiper(".mySwiper2", {
+    effect: "flip", // Add the fade effect: creative  coverflow tile flip cards
+    // grabCursor: true,
+    speed: 300,
+    loop: true,
+    autoplay: {
+    delay: 2500, // 2.5 seconds
+    disableOnInteraction: true,
+    preloadImages:true,
+    
+    // reverseDirection: true
+  },
+  keyboard: true,
+    pagination: {
+      el: ".swiper-pagination2",
+      type: "fraction", //bulletes progressbar
+      
+    },
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+  });
+}
+
+function display(t) {
+  console.log(t);
+
+  // Step 1: Add class 'd-none' to elements with class "resume-btn" if not exists
+  var resumeButtons = document.querySelectorAll(".section3-item");
+  for (var i = 0; i < resumeButtons.length; i++) {
+    if (!resumeButtons[i].classList.contains("d-none")) {
+      resumeButtons[i].classList.add("d-none");
+    }
+  }
+
+  // Step 2: Check the value of t and remove "d-none" from the appropriate element
+  if (t.getAttribute("value") === "experience") {
+    var experienceElement = document.querySelector("#experience");
+    if (experienceElement) {
+      experienceElement.classList.remove("d-none");
+    }
+  } else if (t.getAttribute("value") === "education") {
+    var educationElement = document.querySelector("#education");
+    if (educationElement) {
+      educationElement.classList.remove("d-none");
+    }
+  }
+}
+
 
 function main()
 {
   typedjs();
   AOS.init();
   crousals();
+  crousals2()
+  display(t)
 }
 
